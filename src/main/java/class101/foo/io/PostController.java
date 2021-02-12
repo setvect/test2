891a368ce0bf67aeb0c8884c2935d92b7ea45232
setvect <setvect@naver.com>
2021-02-12 21:57:56 +0900

    검색

--- src/main/java/class101/foo/io/PostController.java
+++ src/main/java/class101/foo/io/PostController.java
@@ -4,7 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -34,12 +39,14 @@ public class PostController {
     // 3. 글 번호로 조회
     // 2-2 글 목록을 페이징하여 반환
     @GetMapping("/post/{id}")
-    public Post getPostById(@PathVariable ("id") Long id) {
+    public Post getPostById(@PathVariable("id") Long id) {
         return postRepository.findById(id).get();
     }
 
 
-
     // 4. 글 내용으로 검색 -> 해당 내용이 포함된 모든 글
-
+    @GetMapping("/search")
+    public List<Post> getPostList(@RequestParam String contents) {
+        return postRepository.findByContentContains(contents);
+    }
 }
